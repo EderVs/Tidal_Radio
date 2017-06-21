d9d586d89d92a7ccd700a094134f064fa055f581
edervs <eedervs.97@gmail.com>
2017-06-20 22:06:35 -0500

    Login with tidal api

--- tidal_radio/users/forms.py
+++ tidal_radio/users/forms.py
@@ -8,5 +8,6 @@ class LoginForm(forms.Form):
     # Tidal's username
     username = forms.CharField(label="Tidal's username")
     # Tidal's password
-    password = forms.CharField(label="Tidal's password")
+    password = forms.CharField(label="Tidal's password",
+        widget=forms.PasswordInput())
 
--- tidal_radio/users/views.py
+++ tidal_radio/users/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+import tidalapi
+
 from . import forms
 
 """
@@ -22,4 +24,16 @@ class LoginView(View):
             'form': form
         }
         return render(request, html, context)
+
+    """
+        Post request of Login
+    """
+    def post(self, request):
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        # Creating a session in tidal api
+        session = tidalapi.Session()
+        # Login
+        session.login(username, password)
+        return HttpResponse(str(session.user.id))
 
